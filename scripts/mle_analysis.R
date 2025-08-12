8f7d944050af5e29bad1f4b7b05f970b01ed90ea
Kevin Boyd <63878894+kevinboyd76@users.noreply.github.com>
2025-08-12 17:54:33 -0500

    Update mle_analysis.R

--- scripts/mle_analysis.R
+++ scripts/mle_analysis.R
@@ -8,14 +8,15 @@ suppressPackageStartupMessages({
   library(cowplot)
   library(ggplot2)
   library(ggrepel)
+  library(rlang)
 })
 
 # -------------------------
 # Capture command-line arguments
 # -------------------------
 args <- commandArgs(trailingOnly = TRUE)
-gene_summary <- strsplit(args[1], " ")[[1]]
-count_summary <- strsplit(args[2], " ")[[1]]
+gene_summary <- args[1]
+count_summary <- args[2]
 design_file <- args[3]
 output_dir <- args[4]
 
@@ -67,11 +68,48 @@ FluteMLE(
 # -------------------------
 # Generate Custom Plots
 # -------------------------
+
+# pull out beta scores
+gdata <- ReadBeta(replicates)
+
+# sanity check: columns exist
+stopifnot(ctrlname %in% names(gdata), treatname %in% names(gdata))
+
 # Simple QC: histogram of beta scores from the MLE gene summary.
-p <- ggplot(replicates, aes(x = beta)) +
-  geom_histogram(bins = 60) +
+p1 <- ggplot(gdata, aes(x = .data[[ctrlname]])) +
+  geom_histogram(bins = 50) +
   theme_bw() +
   labs(title = sprintf("Beta score distribution (%s vs %s)", treatname, ctrlname),
        x = "Beta", y = "Count")
 
-ggsave(out_png, plot = p, width = 8, height = 6, dpi = 150)
+p2 <- ggplot(gdata, aes(x = .data[[treatname]])) +
+  geom_histogram(bins = 50) +
+  theme_bw() +
+  labs(title = sprintf("Beta score distribution (%s vs %s)", treatname, ctrlname),
+       x = "Beta", y = "Count")
+
+# plot histograms them side by side
+out_png <- file.path(output_dir, sprintf("beta_hist_%s_vs_%s.png", treatname, ctrlname))
+hists <- cowplot::plot_grid(p1, p2, labels = c("Control Beta","Treatment Beta"), nrow = 1)
+ggsave(out_png, plot = hists, width = 12, height = 6, dpi = 150)
+
+################################################################################
+
+# methods for normalization
+gdata_cc = NormalizeBeta(gdata, samples=c(ctrlname, treatname), method="cell_cycle")
+gdata_loe = NormalizeBeta(gdata, samples=c(ctrlname, treatname), method="loess")
+
+#to compare density of the non-normalized betas
+p_raw <- DensityView(gdata, samples=c(ctrlname, treatname))
+
+#to compare density of the normalized betas
+p_cc <- DensityView(gdata_cc, samples=c(ctrlname, treatname))
+
+#to compare density of the normalized betas
+p_loess <- DensityView(gdata_loe, samples=c(ctrlname, treatname))
+
+# plot them side by side
+norm_grid <- cowplot::plot_grid(p_raw, p_cc, p_loe, labels = c("NonNormalized", "CellCycle", "Loess"), nrow = 1)
+out_norm_png <- file.path(output_dir, sprintf("beta_norm_%s_vs_%s.png", treatname, ctrlname))
+ggsave(out_norm_png, plot = dens_grid, width = 15, height = 5, dpi = 150)
+
